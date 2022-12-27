76c221ba24ef4418c80c4811562b32706b099bc9
Tadhg McIntyre <mcintyretadhg@gmail.com>
2022-12-27 13:25:55 -0500

    added navbar and added functionality to navigate between created pages from the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,15 +4,15 @@ function NavBar(){
     return(<>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Olde Familiar BarberShop</a>
+                <a className="navbar-brand" href="">Olde Familiar BarberShop</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        <a className="nav-link" href="#">Scheduling</a>
-                        <a className="nav-link" href="#">Menu</a>
+                        <a className="nav-link active" aria-current="page" href="">Home</a>
+                        <a className="nav-link" href="/schedule">Scheduling</a>
+                        <a className="nav-link" href="/menu">Menu</a>
                     </div>
                 </div>
             </div>
