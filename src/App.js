ff6e5604acdb51225ffc7500e1e073238d3cabe2
Tadhg McIntyre <mcintyretadhg@gmail.com>
2022-12-27 13:24:40 -0500

    added react-router-dom functionality with landing, menu, and scheduling pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import LandingPage from './pages/Landing';
+import Menu from './pages/Menu';
+import Schedule from './pages/Schedule';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (<>
+  <BrowserRouter>
     <NavBar/>
-    <div className='body'>
-    
-    </div>
+    <Routes>
+      <Route path='' element={<LandingPage />} />
+      <Route path='/menu' element={<Menu/>}/>
+      <Route path='/schedule' element={<Schedule/>}/>
+    </Routes>
+  </BrowserRouter>
   </>);
 }
 
